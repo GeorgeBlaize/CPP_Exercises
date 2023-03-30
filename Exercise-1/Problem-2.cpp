0e259b93f7f1b278241eb7912ce3ac48a1c9de4b
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-03-30 17:25:56 +0600

    Add files via upload

---
+++ Exercise-1/Problem-2.cpp
@@ -0,0 +1,18 @@
+
+//C++ Program to Print Number Entered by User
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout<<"Enter an integer: ";
+
+    cin>>num;
+
+    cout<<"You entered "<<num;
+
+    return 0;
+}
