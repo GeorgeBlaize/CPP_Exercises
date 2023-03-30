522b6d2d1f520a42d02b41ad370acdc1a69bbdfd
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-03-30 17:42:28 +0600

    Add files via upload

---
+++ Exercise-1/Problem-4.cpp
@@ -0,0 +1,29 @@
+
+//C++ Program to Find Quotient and Remainder
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int dividend,divisor,quotient,remainder;
+
+    cout<<"Enter dividend: ";
+
+    cin>>dividend;
+
+    cout<<"Enter divisor: ";
+
+    cin>>divisor;
+
+    quotient=dividend/divisor;
+
+    remainder=dividend%divisor;
+
+    cout<<"Quotient = "<<quotient<<endl;
+
+    cout<<"Remainder = "<<remainder;
+
+    return 0;
+
+}
