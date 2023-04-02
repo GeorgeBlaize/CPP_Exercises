a7a988256452f527f86f28c2dbec4bbe706d7be9
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-04-02 23:43:17 +0600

    Add files via upload

---
+++ Exercise-1/Problem-14.cpp
@@ -0,0 +1,20 @@
+
+//C++ Program to Generate Multiplication Table
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter a positive integer: ";
+    cin>>num;
+
+    for(int i=1; i<=10; i++)
+    {
+        cout<<num<<" * "<<i<<" = "<<num*i<<endl;
+    }
+
+    return 0;
+
+}
