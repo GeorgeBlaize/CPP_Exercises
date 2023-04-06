9936a2fa01e577f141c078267c78b8da8a35c1b1
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-04-06 23:16:49 +0600

    Add files via upload

---
+++ Exercise-1/Problem-22.cpp
@@ -0,0 +1,17 @@
+
+//C++ Program to Find ASCII Value of a Character
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout<<"Enter a character: ";
+    cin>>ch;
+
+    cout<<"ASCII Value of "<<ch<<" is "<<int(ch);
+
+    return 0;
+}
