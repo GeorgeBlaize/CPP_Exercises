f49bdc86e3e1fe9de31fbbd8b5f762e3409d9322
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-04-06 23:25:11 +0600

    Add files via upload

---
+++ Exercise-1/Problem-23.cpp
@@ -0,0 +1,21 @@
+
+//C++ Program to Multiply two Numbers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   float num1,num2,multiply;
+
+    cout<<"Enter two numbers: ";
+
+    cin>>num1>>num2;
+
+    multiply = num1 * num2;
+    cout<<"Product = "<<multiply;
+
+    return 0;
+
+
+}
