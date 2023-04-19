de6e31e33cc8ceee8579a8c5ea031eaf9b107630
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-04-20 01:22:24 +0600

    Add files via upload

---
+++ Exercise-1/Problem-37.cpp
@@ -0,0 +1,32 @@
+
+//C++ Program to Find G.C.D Using Recursion
+
+#include<iostream>
+using namespace std;
+
+int hcf(int num1,int );
+
+int main()
+{
+
+    int num1,num2;
+
+    cout<<"Enter two positive integer: ";
+    cin>>num1>>num2;
+
+    cout<<"H.C.F of"<<num1<<" & "<<num2<<"is: "<<hcf(num1,num2);
+
+    return 0;
+
+
+
+    return 0;
+}
+
+int hcf(int num1,int num2)
+{
+    if(num2!=0)
+        return hcf(num2,num1%num2);
+        else
+            return num1;
+}
